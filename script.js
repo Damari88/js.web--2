adf9d76365f970b04288504cd7e145f73ab1feff
Damari <damaricruz650@gmail.com>
2023-07-19 15:18:37 -0600

    clase 2 finalizada

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 //listener tipo click
 const btn = document.querySelector("[data-form-btn]");
 
-console.log(btn);
-
-btn.addEventListener("click",  function () {
+const createTask = (evento) => {
+    evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
     console.log(input.value);
-});
+};
+
+
+
+console.log(btn);
+//arrow funtions o funciones anonimas
+btn.addEventListener("click", createTask  );
 
